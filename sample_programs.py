49b73c653fe5f4f01847335253a1957aa566cbf6
tchapman <tarun.chapman@nanoporetech.com>
2024-12-14 12:24:38 +0000

    Converted stored_codon to list which is like a stack (uses pop for previous stored codon things), added a tape stack and ability to load multiple tapes etc

--- sample_programs.py
+++ sample_programs.py
@@ -1,9 +1,11 @@
 import the_genetic_computer
 
-program = """
-                AAAATAAAACCCCGAATAAGG
-                    """
+program = """AAACGGTTACGGTCCCGGTGACGGTGACGGTGGCGGAGACGGAAACGGGCGCGGTGGCGGGATCGGTGACGGTCATTT"""
+program_two = "AAAATAAATTGCTTT"
 
 tape = the_genetic_computer.create_tape(program)
+tape_2 = the_genetic_computer.create_tape(program_two)
 
-the_genetic_computer.read_tape(tape, debug_mode=False)
+tape_list = the_genetic_computer.initialise_tape_list([tape_2,tape])
+
+the_genetic_computer.read_tape(tape_list, debug_mode=False)
